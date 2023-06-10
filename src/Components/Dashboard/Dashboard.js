3e0d3bf18738701d516037bf200a9773d811e6b0
devalleytech <codewithghan@gmail.com>
2023-06-10 21:43:59 +0530

    change in designs

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -10,18 +10,18 @@ const Dashboard = () => {
     return (
       <>
         {(checkForoutlet==="/dashboard") && <div className="">
-          <div className="jumbotron text-center mt-4 py-4">
+          <div className="jumbotron text-center mt-2 py-4">
             <h1>LMS Dashboard  </h1>
           </div>
-          <div className="container mt-4 py-4">
+          <div className="container py-2">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-sm-3 mx-4 card p-4">
-                <div className="row ">
+              <div className="col-sm-3 mx-4 card">
+               <div className="row mt-2 py-2 px-4">
                   <div className="col-sm-12 mb-2 title">
                     <Link to="/dashboard/list"><h3>Users</h3></Link>
                     <span className="badge bg-primary badge-pill">14</span>
                   </div>
-                  <ul className="list-group mt-4">
+                  <ul className="list-group mt-2 py-2">
                     <li className="list-group-item d-flex justify-content-between align-items-center">
                       Cras justo odio
                       <span className="badge badge-primary badge-pill">14</span>
@@ -41,19 +41,57 @@ const Dashboard = () => {
                   </ul>
                 </div>
               </div>
-              <div className="col-sm-3 mx-4 card p-4">
-                <h3>Librarian</h3>
-                <div className="py-3"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <div className="col-sm-3 mx-4 card">
+                <div className="row mt-2 py-2 px-4">
+                  <div className="col-sm-12 mb-2 title">
+                    <Link to="/dashboard/list"><h3>Users</h3></Link>
+                    <span className="badge bg-primary badge-pill">14</span>
+                  </div>
+                  <ul className="list-group mt-2 py-2">
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Cras justo odio
+                      <span className="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Dapibus ac facilisis in
+                      <span className="badge badge-primary badge-pill">2</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Morbi leo risus
+                      <span className="badge badge-primary badge-pill">1</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Morbi leo risus
+                      <span className="badge badge-primary badge-pill">1</span>
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <div className="col-sm-3 mx-4 card  p-4">
-                <h3>Books</h3>
-                <div className="py-3"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <div className="col-sm-3 mx-4 card">
+               <div className="row mt-2 py-2 px-4">
+                  <div className="col-sm-12 mb-2 title">
+                    <Link to="/dashboard/list"><h3>Users</h3></Link>
+                    <span className="badge bg-primary badge-pill">14</span>
+                  </div>
+                  <ul className="list-group mt-2 py-2">
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Cras justo odio
+                      <span className="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Dapibus ac facilisis in
+                      <span className="badge badge-primary badge-pill">2</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Morbi leo risus
+                      <span className="badge badge-primary badge-pill">1</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                      Morbi leo risus
+                      <span className="badge badge-primary badge-pill">1</span>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
             {/* <Outlet />  */}
